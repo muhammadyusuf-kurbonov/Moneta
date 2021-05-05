6421cafefd281e246a62175fb467cde68375ab28
HarshBarash <barashEU@yandex.ru>
2021-05-05 19:14:13 +0500

    Add data to Firebase

--- app/src/main/java/harshbarash/github/io/HomeFragment.java
+++ app/src/main/java/harshbarash/github/io/HomeFragment.java
@@ -19,17 +19,16 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        define = (ImageButton) container.findViewById(R.id.define);
+        define = v.findViewById(R.id.define);
         define.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(HomeFragment.this, Define.class);
-                startActivity(i);
-                finish();
+                startActivity(new Intent(getActivity(), Define.class));
+
             }
         });
-
+        return v;
     }
 }
