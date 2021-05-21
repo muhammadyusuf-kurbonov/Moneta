821ed49612f69d9194c7583ff17233c8d436892e
HarshBarash <barashEU@yandex.ru>
2021-05-21 22:30:02 +0500

    Step 2 chat

--- app/src/main/java/harshbarash/github/io/DialogFragment.java
+++ app/src/main/java/harshbarash/github/io/DialogFragment.java
@@ -1,18 +1,71 @@
 package harshbarash.github.io;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class DialogFragment extends Fragment {
+
+    Button login, register;
+
+    FirebaseUser user;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dialog, container, false);
+        View view = inflater.inflate(R.layout.activity_start, container, false);
+        Button login = (Button) view.findViewById(R.id.loginBtn);
+        Button register = (Button) view.findViewById(R.id.registerbtn);
+
+
+
+        //перенаправляем
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.loginBtn:
+                        Intent intent = new Intent(DialogFragment.this.getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.registerbtn:
+                        Intent intenttwo = new Intent(DialogFragment.this.getActivity(), RegisterActivity.class);
+                        startActivity(intenttwo);
+                        break;
+                }
+            }
+        };
+        login.setOnClickListener(onClickListener);
+        register.setOnClickListener(onClickListener);
+
+        return view;
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            Intent intent = new Intent(DialogFragment.this.requireContext(), ChatActivity.class);
+            startActivity(intent);
+
+//            Intent intent = new Intent();
+//            intent.setClass(getActivity(), ChatActivity.class);
+//            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/harshbarash/github/io/RegisterActivity.java
@@ -0,0 +1,147 @@
+package harshbarash.github.io;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    MaterialEditText et_username, et_password, et_email;
+    Button registerbtn;
+    Toolbar toolbar;
+
+    String username, email, password;
+
+    FirebaseAuth mAuth;
+
+    DatabaseReference reference;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        toolbar = findViewById(R.id.toolbarregis);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        et_username = findViewById(R.id.reg_username);
+        et_email = findViewById(R.id.reg_email);
+        et_password = findViewById(R.id.reg_password);
+        registerbtn = findViewById(R.id.register_Account_btn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+        registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email = et_email.getText().toString();
+                password = et_password.getText().toString();
+                username = et_username.getText().toString();
+
+
+                if (TextUtils.isEmpty(email)) {
+                    et_email.setError("Введите почту");
+                } else if (TextUtils.isEmpty(username)) {
+                    et_username.setError("Введите имя пользователя");
+
+                } else if (TextUtils.isEmpty(password)) {
+                    et_password.setError("Введите пароль");
+
+                } else if (password.length() < 6) {
+
+                    et_password.setError("Минимум 6 символов");
+                } else {
+
+
+                    registerUser(username, password, email);
+                }
+
+            }
+        });
+
+
+
+
+
+    }
+
+    private void registerUser(final String username, String password, final String email) {
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    reference = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+
+
+                    if (user!=null) {
+
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("username", username);
+                        hashMap.put("email", email);
+                        hashMap.put("id", user.getUid());
+                        hashMap.put("imageURL", "default");
+                        hashMap.put("status", "offline");
+
+
+                        reference.setValue(hashMap).addOnCompleteListener(task1 -> {
+
+
+                            if (task1.isSuccessful()) {
+                                Toast.makeText(RegisterActivity.this, "Успешный вход", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
+
+
+
+
+
+                    }
+
+
+                }
+
+
+
+            }
+        });
+
+    }
+
+
+}
