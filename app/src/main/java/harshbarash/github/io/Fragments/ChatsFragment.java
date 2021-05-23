4311b217b12031c4a00fbecb35462ff2739279f5
Muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2021-05-23 06:14:56 +0500

    moved to onViewCreated

--- app/src/main/java/harshbarash/github/io/Fragments/ChatsFragment.java
+++ app/src/main/java/harshbarash/github/io/Fragments/ChatsFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -40,14 +41,18 @@ public class ChatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_chats, container, false);
+        return inflater.inflate(R.layout.fragment_chats, container, false);
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         userlist = new ArrayList<>();
 
         recyclerView = view.findViewById(R.id.chat_recyclerview_chatfrag);
         mAdapter = new UserAdapter(getContext(), new ArrayList<>(), true);
-        recyclerView.setAdapter(mAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(mAdapter);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -71,7 +76,6 @@ public class ChatsFragment extends Fragment {
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
-        return view;
     }
 
     // recommended lowerCase for fields and methods
